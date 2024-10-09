d8f3e567b6f777c606f64d2cd94e776240ed44cf
Santi543 <santioldani08@gmail.com>
2024-10-09 22:39:11 +0000

    FIX: Punto_D fixeo de /cliente/

--- RTA_SCRIPT_Examen_20241007/Punto_D.sh
+++ RTA_SCRIPT_Examen_20241007/Punto_D.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo mkdir -p /Estructura_Asimetrica/{correo/{carteros_{1..10},cartas_{1..100}},clientes/cartas_{1..100}}
+sudo mkdir -p ~/Estructura_Asimetrica/{correo/{carteros_{1..10},cartas_{1..100}},cliente/cartas_{1..100}}
